ee048f82aa73f35efbf53732dc06beb5f1dab8f6
Spencer Hulse <hulse.spencer@gmail.com>
2022-07-28 15:25:05 -0400

    Created a quick rock, paper, scissors game.

---
+++ rock-paper-scissors/main.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+  user = input("'r' for rock, 'p' for paper, 's' for scissors")
+  computer = random.choice(["r", "p", "s"])
+
+  if user == computer:
+    return "Draw!"
+
+  if is_win(user, computer):
+    return"You win!"
+
+  return "You lose!"
+
+def is_win(player, opponent):
+  if (player == "r" and opponent == "p") or \
+  (player == "p" and opponent == "s") or \
+  (player == "s" and opponent == "r"):
+    return True
+
+play()
